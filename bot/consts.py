e628f4cb4755873382414ef684a3b01adc9490ae
Andrey Martinich <aimartinich@edu.hse.ru>
2025-10-12 18:30:08 +0300

    add consts.py

---
+++ bot/consts.py
@@ -0,0 +1,12 @@
+ANIME_TAGS = {
+    "sfw": [
+        "waifu", "neko", "shinobu", "megumin", "bully", "cuddle",
+        "cry", "hug", "awoo", "kiss", "lick", "pat", "smug", "bonk",
+        "yeet", "blush", "smile", "wave", "highfive", "handhold",
+        "nom", "bite", "glomp", "slap", "kill", "kick", "happy",
+        "wink", "poke", "dance", "cringe"
+    ],
+    "nsfw": ["waifu", "neko", "blowjob"]
+}
+
+NSFW_EMOJI_TRIGGERS = {'🥵', '😈', '💋', '🍌', '🍑', '🍆'}
